942fae3d5dfe242a44143e81a8b4950361b8e597
Hélvio Filho <helviosvf@gmail.com>
2022-09-18 22:32:44 -0300

    Chore: changed from fetch to axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { useEffect, useState } from 'react';
-
+import axios from 'axios';
 import { GameBanner } from './components/GamerBanner';
 import './styles/main.css';
 
@@ -21,10 +21,9 @@ function App() {
   const [games, setGames] = useState<GameProps[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data);
+    axios('http://localhost:3333/games')
+      .then(response => {
+        setGames(response.data);
       });
   }, []);
   return (
